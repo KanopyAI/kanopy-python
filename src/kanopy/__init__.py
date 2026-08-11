830f8415250502df75bdfeea830557eb4a3530f3
Nicolas Pfitzer <nicolas@pfitzer.org>
2026-08-11 14:26:16 +0000

    Release version 0.5.0

--- src/kanopy/__init__.py
+++ src/kanopy/__init__.py
@@ -5,4 +5,4 @@ from .errors import KanopyError, KanopyUploadError
 from .models import Page
 
 __all__ = ["DEFAULT_BASE_URL", "Kanopy", "KanopyError", "KanopyUploadError", "Page"]
-__version__ = "0.4.0"
+__version__ = "0.5.0"
